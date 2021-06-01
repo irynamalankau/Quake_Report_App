ef65e9481d00e363ad9953734983ba348927f487
ira <ira@ira>
2021-05-31 21:46:16 -0500

    add color to magnitude view

--- app/src/main/java/com/example/quakereport/MyRecyclerViewAdapter.java
+++ app/src/main/java/com/example/quakereport/MyRecyclerViewAdapter.java
@@ -1,7 +1,9 @@
 package com.example.quakereport;
 
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,9 +14,12 @@ import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import android.graphics.drawable.GradientDrawable;
+
+
 
 public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder> {
-
+    private Context mContext;
     private final ArrayList<Earthquake> mEarthquakes;
 
     //constructor
@@ -24,6 +29,7 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        mContext = parent.getContext();
         return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false));
     }
 
@@ -44,7 +50,6 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
         return dateFormat.format(dateObject);
     }
 
-
      // Return the formatted date string (i.e. "4:30 PM") from a Date object.
     private String formatTime(Date dateObject) {
         SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
@@ -56,6 +61,46 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
         DecimalFormat formatter = new DecimalFormat("0.0");
         String formattedMagnitude = formatter.format(earthquake.getMagnitude());
         return formattedMagnitude;
+    }
+
+    //Return specific magnitude color
+    private int getMagnitudeColor(double magnitude) {
+        int magnitudeColorResourceId;
+        int magnitudeFloor = (int) Math.floor(magnitude);
+        switch (magnitudeFloor) {
+            case 0:
+            case 1:
+                magnitudeColorResourceId = R.color.magnitude1;
+                break;
+            case 2:
+                magnitudeColorResourceId = R.color.magnitude2;
+                break;
+            case 3:
+                magnitudeColorResourceId = R.color.magnitude3;
+                break;
+            case 4:
+                magnitudeColorResourceId = R.color.magnitude4;
+                break;
+            case 5:
+                magnitudeColorResourceId = R.color.magnitude5;
+                break;
+            case 6:
+                magnitudeColorResourceId = R.color.magnitude6;
+                break;
+            case 7:
+                magnitudeColorResourceId = R.color.magnitude7;
+                break;
+            case 8:
+                magnitudeColorResourceId = R.color.magnitude8;
+                break;
+            case 9:
+                magnitudeColorResourceId = R.color.magnitude9;
+                break;
+            default:
+                magnitudeColorResourceId = R.color.magnitude10plus;
+                break;
+        }
+        return ContextCompat.getColor(mContext, magnitudeColorResourceId);
     }
 
     // Return array of strings that contains location primary and location offset for the current earthquake
@@ -92,27 +137,37 @@ public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAd
         }
 
         public void bindPlace(Earthquake currentEarthquake) {
+                   //MAGNITUDE
+            // Set the proper background color on the magnitude circle.
+            // Fetch the background from the TextView, which is a GradientDrawable.
+            GradientDrawable magnitudeCircle = (GradientDrawable) mMagnitude.getBackground();
+            // Get the appropriate background color based on the current earthquake magnitude
+            int magnitudeColor = getMagnitudeColor(currentEarthquake.getMagnitude());
+            // Set the color on the magnitude circle
+            magnitudeCircle.setColor(magnitudeColor);
+            //Set text to the magnitude view
             mMagnitude.setText(formatMagnitude(currentEarthquake));
+
+                    //LOCATION
             String location = currentEarthquake.getLocation();
-
+            //get location formatted
             String[] formattedLocationArray = formatLocation(location);
-
+            //set the textView
             mLocationOffset.setText(formattedLocationArray[0]);
             mLocationPrimary.setText(formattedLocationArray[1]);
 
+                    //DATE
             // Create a new Date object from the time in milliseconds of the earthquake
             Date dateObject = new Date(currentEarthquake.getTimeInMilliseconds());
             //get date formatted into string
             String formattedDate = formatDate(dateObject);
             //set the textView
             mDate.setText(formattedDate);
-
             //get time formatted
             String formattedTime = formatTime(dateObject);
+            //set the textView
             mTime.setText(formattedTime);
         }
-
-
 
         @Override
         public String toString() {
